29e4299d9c993c8145edc9b38a9803d1635adc33
blakebrdak <bbrdak@umich.edu>
2023-04-05 12:35:49 -0400

    added idfk calculation to reduce2 output

--- inverted_index/reduce2.py
+++ inverted_index/reduce2.py
@@ -6,6 +6,7 @@ https://github.com/eecs485staff/madoop/blob/main/README_Hadoop_Streaming.md
 """
 import sys
 import itertools
+import math
 
 
 def reduce_one_group(key, group):
@@ -16,8 +17,16 @@ def reduce_one_group(key, group):
     for row in group:
         word, doc_id = row.strip().split('\t')
         docs.append(doc_id)
+    
+    # Calculate inverse document frequency
+    idfk = -1  # init val outside of context mgr
+    with open("total_document_count.txt") as count_doc:
+        total_docs = count_doc.readline() # Read the count
+        docs_set = set(docs)
+        idfk = math.log10(int(total_docs) / len(docs_set))
     string = ' '.join(docs) # Convert list to string
-    print(f'{key}\t{string}')
+
+    print(f'{key}\t{idfk} {string}')
 
 
 def keyfunc(line):
