e4806c064b7eadb09db53b80d527b53ea8a78694
blakebrdak <bbrdak@umich.edu>
2023-04-06 15:53:27 -0400

    flask reads all necessary data, next is implementing the endpoints.

--- index_server/index/api/helpers.py
+++ index_server/index/api/helpers.py
@@ -8,17 +8,65 @@ import index
 def load_index():
     """Load the inverted index, stopwords, and PageRank files."""
     # THESE ARE GLOBAL VARIABLES THAT ARE SHARED.
-    # TODO: STORE THESE IN A USABLE WAY IN THE ENDPOINTS.
     # Reference the vars with index.<varname> in main.
+
     index.index_file = {}
-    index.stopwords = []  # List of all stopwords
-    index.pagerank = {}  # Maps key doc_id to value pagerank score
+    # This is a dictionary of dictionaries (of dictionaries). 
+    # each word has an associated dictionary of 
+    # each document that it appears in, containing the
+    # frequency and norm factor for that document. 
+    # Outer dictionary also has idf of the word.
+    #
+    # --- EXAMPLE USAGE --- #
+    # index.index_file[<word>] will return:
+    # {
+    #   'idf_score': <idf score of the word>
+    #   <doc_id1>: {
+    #       "frequency": <freq in doc_id1>
+    #       "norm_factor": <norm_factor for doc doc_id1>
+    #   }
+    #   <doc_id2>: {
+    #       ... same structure as prev
+    #   }
+    #   ... and so on
+    # }
+    # I really hope this is a smart way to store the info after
+    # all this documentation i did lmao 
+    # -- Blake <3
+
+    # List of all stopwords
+    index.stopwords = []
+
+    # Maps key doc_id to value pagerank score
+    index.pagerank = {}
 
     # Load inverted index
     idx_path = 'index_server/index/inverted_index/' + index.app.config['INDEX_PATH']
     with open(idx_path) as fin:
         for row in fin:
-            # TODO How to store index in memory?
+            # initial setup before looping each doc
+            row = row.strip().split()
+            word = row[0].strip()
+            idf_score = row[1].strip()
+            index.index_file[word] = {}
+            index.index_file[word]['idf_score'] = float(idf_score)
+
+            # Loop over all groups of 3 and assign them correctly
+            row = row[2:]  # Remove first 2 items to make iterating simpler.
+            doc_id = -1
+            frequency = -1
+            # iterate and split each group of 3 into dict
+            for idx, item in enumerate(row):
+                if idx % 3 == 0:
+                    doc_id = item.strip()
+                elif idx % 3 == 1:
+                    frequency = float(item.strip())
+                else:  # When idx % 3 = 2
+                    index.index_file[word][doc_id] = {}
+                    index.index_file[word][doc_id]['frequency'] = frequency
+                    index.index_file[word][doc_id]['norm_factor'] = float(item.strip())
+
+
 
     # Load stopwords
     with open('index_server/index/stopwords.txt') as fin:
--- index_server/index/api/main.py
+++ index_server/index/api/main.py
@@ -6,8 +6,10 @@ import index
 @index.app.route('/api/v1/', methods=['GET'])
 def routes():
     # EXAMPLE OF HOW TO REFERENCE THE GLOBALS
-    return(index.index_file["smelting"])
+    # TODO: IMPLEMENT ME
+    return(index.index_file['smelting'])
 
 @index.app.route('/api/v1/hits/', methods=['GET'])
 def hits():
+    # TODO: IMPLEMENT ME
     return("IMPLEMENT ME")
