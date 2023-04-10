f16ddb6851a222af007af1c374c330f153571d6c
blakebrdak <bbrdak@umich.edu>
2023-04-10 16:34:18 -0400

    Search server progress. Finds matching docs but doesnt sort or print yet.

--- search_server/search/config.py
+++ search_server/search/config.py
@@ -1,5 +1,14 @@
+"""Search Server Configuration."""
+
+import pathlib
+
 SEARCH_INDEX_SEGMENT_API_URLS = [
     "http://localhost:9000/api/v1/hits/",
     "http://localhost:9001/api/v1/hits/",
     "http://localhost:9002/api/v1/hits/",
 ]
+
+# File Upload to var/uploads/
+SEARCH_ROOT = pathlib.Path(__file__).resolve().parent.parent
+
+DATABASE_FILENAME = SEARCH_ROOT/'sql'/'search.sql'
--- search_server/search/views/main.py
+++ search_server/search/views/main.py
@@ -3,6 +3,8 @@
 URLS Included:
 /
 """
+import requests
+import json
 import threading
 import flask
 import search
@@ -17,9 +19,26 @@ def show_index():
     weight = 0.5
     if 'w' in flask.request.args:
         weight = flask.request.args['w']
-    context = {'query': query}
+    context = {'query': query, 'weight': weight}
+    search_urls = []
+    for url in search.app.config['SEARCH_INDEX_SEGMENT_API_URLS']:
+        processed_query = query.replace(' ', '+')
+        search_urls.append(url + '?w=' + weight + '&q=' + processed_query)
+
+    # Call Index Servers and gather results
+    # This works right now (it gathers all the results)
+    # BUT it still needs to sort results, actually show the data
+    # on the page, and be multi-threaded. 
+    results = []
+    for url in search_urls:
+        results = results + json.loads(call_index(url))['hits']
+    print("results = ", results)
+
+    # Make requests to the index servers
     return flask.render_template("index.html", **context)  # Add **context eventually
 
 
 def call_index(route):
     """Make a call to the index server, return results."""
+    r = requests.get(route)
+    return r.text
