15c4e84908e61afd1c7026564ca77bd50fd95c42
blakebrdak <bbrdak@umich.edu>
2023-04-10 13:19:35 -0400

    small additions

--- search_server/search/views/main.py
+++ search_server/search/views/main.py
@@ -1,13 +1,25 @@
-"""SEARCH SERVER
+"""Search Server Views. 
 
 URLS Included:
 /
 """
-
+import threading
 import flask
 import search
 
 
 @search.app.route('/')
 def show_index():
-    return flask.render_template("index.html")  # Add **context eventually
+    # Gather data from form
+    query = ""
+    if 'q' in flask.request.args:
+        query = flask.request.args.get('q')  # THIS ONLY GETS FIRST WORD RIGHT NOW?
+    weight = 0.5
+    if 'w' in flask.request.args:
+        weight = flask.request.args['w']
+    context = {'query': query}
+    return flask.render_template("index.html", **context)  # Add **context eventually
+
+
+def call_index(route):
+    """Make a call to the index server, return results."""
